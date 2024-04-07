d0391ca6a4ba1e5c16a5e4d33dadaab6b709cbdb
Emil Smolinski <s28014@pjwstk.edu.pl>
2024-04-08 01:47:11 +0200

    Update 1

---
+++ test2/test2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("siema");
+        }
+    }
+}
